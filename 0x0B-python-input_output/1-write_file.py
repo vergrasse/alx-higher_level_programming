628d712357e696ccc5f0b6d746f9b148416cab08
vergrasse <12alimaabid132@gmail.com>
2024-02-06 11:21:28 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """write_file module.
 
-Contains a function that writes a string to a text file.
+Contains a function that writes a text file.
 """
 
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
-    If the file doesn’t exist, it should be created.
-    The function should overwrite the content of the file if it already exists.
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
     """
     with open(filename, 'w') as f:
         return f.write(text)
