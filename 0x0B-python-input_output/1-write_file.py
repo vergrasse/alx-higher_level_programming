ea8223e3f1fd7f35213d6739c94becc83c077e3b
vergrasse <12alimaabid132@gmail.com>
2024-02-06 11:08:15 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""write-file module
+"""write_file module.
 
-Contains a function that whrite a text file.
+Contains a function that writes a string to a text file.
 """
 
 
-def append_write(filename="", text=""):
-    """Appends a string at the end of a text file (UTF8) and returns the number of characters added."""
-    with open(filename, mode='a', encoding='utf-8') as file:
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    # Open the file in write mode (overwriting existing content) or create it if it doesn't exist
+    with open(filename, mode='w', encoding='utf-8') as file:
+        # Write the text to the file
         file.write(text)
+        # Return the number of characters written
         return len(text)
